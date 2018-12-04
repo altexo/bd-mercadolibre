cb8eeecf54b2c19216db55a3172a46a42aae334a
altexo <altexo94@gmail.com>
2018-12-04 16:56:54 -0700

    Login

--- public/js/ml.js
+++ public/js/ml.js
@@ -2,20 +2,30 @@ $( document ).ready(function() {
     console.log( "ready!" );
     MELI.init({ client_id: 6677614414680820 });
 
-    if (!MELI.getToken()) {
+  //  if (!MELI.getToken()) {
     	//var token = MELI.getToken();
-    	MELI.get("/users/me", {}, function(data) {
-	    	console.log("Hello " + data[2].first_name);
-	  	});
+    // 	MELI.get("/users/me", {}, function(data) {
+	   //  	console.log("Hello " + data[2].first_name);
+	  	// });
 		//console.log('Token: '+token)
-	}
+//	}
+if (!localStorage.getItem("token")) {
+	console.log("Token in localStorage: "+token)
+}
+
 });
 
 $("#login-button").click(function(){
 	 MELI.login(function() {
-	 	console.log(MELI.getToken());
-	  	// MELI.get("/users/me", {}, function(data) {
+	 	
+	 	  	// MELI.get("/users/me", {}, function(data) {
 	   //  	alert("Hello " + data[2].first_name);
 	  	// });
+	  	MELI.get("/users/me", {}, function(data) {
+	    	console.log("Login data: "+data)
+	    	console.log("token: "+MELI.getToken());
+	    	var storage = [token: MELI.getToken()];
+	  	});
+	  	localStorage.setItem("token", storage);
 	});
 });
