a29cab192e5a7551ae0eba8b27b1317cee9878ba
altexo <altexo94@gmail.com>
2018-12-04 16:36:41 -0700

    Login

--- public/js/ml.js
+++ public/js/ml.js
@@ -3,18 +3,18 @@ $( document ).ready(function() {
     MELI.init({ client_id: 6677614414680820 });
 
     if (!MELI.getToken()) {
-    	var token = MELI.getToken();
-    	MELI.get("/users/me", null, function(data) {
-	  	console.log(data);
-		});
-		console.log('Token: '+token)
+    	//var token = MELI.getToken();
+    	MELI.get("/users/me", {}, function(data) {
+	    	console.log("Hello " + data[2].first_name);
+	  	});
+		//console.log('Token: '+token)
 	}
 });
 
 $("#login-button").click(function(){
 	 MELI.login(function() {
-	  	MELI.get("/users/me", {}, function(data) {
-	    	alert("Hello " + data[2].first_name);
-	  	});
+	  	// MELI.get("/users/me", {}, function(data) {
+	   //  	alert("Hello " + data[2].first_name);
+	  	// });
 	});
 });
