1a14f13df43f98bb42157845179845e9ec19ac67
altexo <altexo94@gmail.com>
2018-12-04 15:28:20 -0700

    Https ML script (Change to local)

--- resources/views/mercadojs.blade.php
+++ resources/views/mercadojs.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @endsection
 @section('scripts')
-<script src="https://static.mlstatic.com/org-img/sdk/mercadolibre-1.0.4.js"></script>
+<script src="{{asset('mercadolibre-1.0.4.js')}}"></script>
 <script src="https://a248.e.akamai.net/secure.mlstatic.com/org-img/sdk/mercadolibre-1.0.4.js"></script>
 <script src="{{asset('js/ml.js')}}" type="text/javascript" charset="utf-8" async defer></script>
 @endsection
