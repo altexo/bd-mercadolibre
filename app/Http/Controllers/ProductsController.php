5af3dfc6af49ed02e8a9c0c0c137f40292bc54b6
altexo <altexo94@gmail.com>
2019-01-03 11:27:24 -0700

    Publish especific ml id

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -252,7 +252,7 @@ class ProductsController extends Controller
     public function publishAll(){
         $response = "";
         $error = false;
-        $response = Ml_data::with('pictures', 'shipping', 'tags', 'products')->first();
+        $response = Ml_data::with('pictures', 'shipping', 'tags', 'products')->where('ml_data.id', 6205)->get();
         //$response = Pictures::first();
         return response()->json(['error'=> $error,'response'=> $response]);
     }
