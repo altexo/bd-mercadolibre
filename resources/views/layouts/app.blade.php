7828d4c4e52decdac0ec42738fc13dc6a4c011e6
Antonio Valle <vallegalvez.93@gmail.com>
2019-05-25 13:07:45 -0400

    Boton inicio en navbar link a home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@ body{
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="{{route('home')}}">Inicio <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
               <a class="nav-link" href="{{route('products.view')}}">Productos</a>
@@ -72,7 +72,8 @@ body{
               </li> --}}
             </ul>
             <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" aria-label="Search">
+              <input class="form-control mr-sm-2" type="search" 
+              aria-label="Search">
               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
             </form>
 
