3dcdddd049f942910c96d69837e3826aa35d034d
altexo <alejandro.riosyb@gmail.com>
2019-07-10 11:50:07 -0600

    Task/Get Token with meli

---
+++ app/Http/AppServices/UpdateInML.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\AppServices;
+use GuzzleHttp\Client;
+use GuzzleHttp;
+use DB;
+use App\User;
+
+class UpdateInML{
+    
+    private function getToken(){
+        $token = User::find(11);
+        return $token->ml_token;
+    }
+
+    public function updatePrice($asin, $price, $status){
+        
+        $appId = ENV('APP_ID');
+        $secretKey = ENV('SECRET_KEY');
+
+        $meli = new Meli($appId, $secretKey);
+        $token = $this->getToken();
+
+        $params = array('sku' => $asin,'access_token' => $token);
+        $price = 1.30 * $price;
+        $price = round($price);
+
+        try {
+            $result = $meli->get('/users/'.ENV('SELLER_ID').'/items/search', $params, true);
+            $ml_id = $result['body']['results'][0];
+            try {
+                $params = ['access_token' => $token ];
+                $body = ['price' => $price, 'status' => $status];
+                $result = $meli->put('/items/'.$ml_id, $body,$params);
+                if ($result['httpCode'] == 200) {
+                    return true;
+                }else{
+                    return json_decode(json_encode($result), true);
+                }
+                
+            } catch (\Exception $th) {
+                return $th;
+            }
+        } catch (\Exception $th) {
+            return $th;
+            //echo "Error retriving From ML, SKU: ".$asin."\n";
+        }
+
+    }
+
+    public function disableProduct($asin){
+        
+        $appId = ENV('APP_ID');
+        $secretKey = ENV('SECRET_KEY');
+
+        $meli = new Meli($appId, $secretKey);
+        $token = $this->getToken();
+
+        $params = array('sku' => $asin,'access_token' => $token);
+
+        try {
+            $result = $meli->get('/users/'.ENV('SELLER_ID').'/items/search', $params, true);
+            $ml_id = $result['body']['results'][0];
+            try {
+                $params = ['access_token' => $token ];
+                $body = ['status' => 'paused'];
+                $result = $meli->put('/items/'.$ml_id, $body,$params);
+                if ($result['httpCode'] == 200) {
+                    return true;
+                }else{
+                    return json_decode(json_encode($result), true);
+                }
+                
+            } catch (\Exception $th) {
+                return $th;
+            }
+        } catch (\Exception $th) {
+            return $th;
+            //echo "Error retriving From ML, SKU: ".$asin."\n";
+        }
+
+    }
+
+
+
+}
+
+?>
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Attributes;
+use App\Http\AppServices\Meli;
 class HomeController extends Controller
 {
     /**
@@ -23,7 +24,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('mercadojs');
+
+         
+        $appId = ENV('APP_ID');
+        $secretKey = ENV('SECRET_KEY');
+        $siteId = ENV('SITE_ID');
+        $redirectURI = ENV('REDIRECT_URI');
+        
+        $meli = new Meli($appId, $secretKey);
+
+        echo '<p><a alt="Login using MercadoLibre oAuth 2.0" class="btn" href="' . $meli->getAuthUrl($redirectURI, Meli::$AUTH_URL[$siteId]) . '">Authenticate</a></p>';
+        if (isset($_GET['code'])) {
+            $user = $meli->authorize($_GET['code'], $redirectURI);
+            echo $user['body']->access_token."<br>";
+            echo time() + $user['body']->expires_in."<br>";
+            echo $user['body']->refresh_token."<br>";
+        }
+        //return view('mercadojs');
     }
 
  
