30eee69ce779555d5a556a627780a6d136365c6b
altexo <alejandro.riosyb@gmail.com>
2019-08-20 15:55:48 -0600

    Task/get msjs

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\MessagesML;
+use App\Http\AppServices\MessagesML;
 class MessagesController extends Controller
 {
     public function index(){
